d5656e3df22cc50660a9a110b4e7dc27cb2f494b
Jonas4884 <hei.jonatana@gmail.com>
2023-02-18 19:20:29 +0300

    chore : complete to do test

--- src/tests/ListItem.test.jsx
+++ src/tests/ListItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { getByTestId, render } from "@testing-library/react";
 
 import { ListItem } from "../ListItem";
 
@@ -46,9 +46,33 @@ describe('ListItem', () => {
         expect(node.children).toHaveLength(1);
     });
     
-    it('callback is called', () => {});
+    it('callback is called', () => {
+        const onCheck = jest.fn()
+        const callBack = (event, item, i) => onCheck(event, item, i) ;
+        const {getByRole} = render(
+            <ListItem
+            id='list-item-1'
+            checkable = {true}
+            onCheck={callBack}
+            item='Lorem ipsum dolor sit amet consectetur'
+        />
+        )
+        const node = getByRole("checkbox");
+        expect(node).not.toBeChecked();
+    });
 
-    it('callback is not called when not checkable', () => {});
+    it('callback is not called when not checkable', () => {
+        const {getByTestId} = render(
+            <ListItem
+            id='list-item-1'
+            checkable = {false}
+            item='Lorem ipsum dolor sit amet consectetur'
+        />
+        )
+        const node = getByTestId('test-list-item-1-container');
+        expect(node.children).toHaveLength(1);
+
+    });
 
     it('matches saved snapshot', () => {
         const tree = render(
