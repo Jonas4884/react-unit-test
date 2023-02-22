f9139cc66acce34c5c1d1c87e84529dc17724ee4
Jonas4884 <hei.jonatana@gmail.com>
2023-02-22 11:27:31 +0300

    chore : add new label test

--- cypress/e2e/addTodo.cy.js
+++ cypress/e2e/addTodo.cy.js
@@ -5,9 +5,9 @@ describe('template spec', () => {
   });
 
   it('add new todo in input', () => {
- 
+    cy.get("#todo-task>ul>li>input")
   })
   it('check if the task is todo', () => {
-      cy.get("#todo-task.ul")
+      cy.get("#todo-task>ul>li>label").contains('new todo 1')
   });
 })
