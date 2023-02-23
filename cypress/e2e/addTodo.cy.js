5e26af030509ed6e7539f981cb8cae1d4f9f4a6d
Jonas4884 <hei.jonatana@gmail.com>
2023-02-23 13:08:04 +0300

    chore : add final test

--- cypress/e2e/addTodo.cy.js
+++ cypress/e2e/addTodo.cy.js
@@ -5,7 +5,11 @@ describe('template spec', () => {
   });
 
   it('add new todo in input', () => {
-    cy.get("#todo-task>ul>li>input")
+    cy.get("#todo-task>ul").as('checkbox_input')
+    cy.get('#todo-task>ul>li>input').check()
+    cy.get('#todo-task>ul>li').children().should('have.length', 0)
+    cy.get('#done-task>ul>li').children().should('have.value', 'new todo 1')
+
   })
   it('check if the task is todo', () => {
       cy.get("#todo-task>ul>li>label").contains('new todo 1')
